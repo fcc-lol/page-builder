7972dda82d02c77205d5e052a99dc3c717a32c3b
Leo Mancini <mancini@leo.gd>
2024-06-28 17:43:25 -0400

    cleaning up

--- src/app/page/[slug]/page.js
+++ src/app/page/[slug]/page.js
@@ -2,24 +2,28 @@
 import { db } from "../../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
-async function fetchFromFirestore(db, id) {
-  const citiesCol = collection(db, "pages");
-  const citySnapshot = await getDocs(citiesCol);
-  const cityDoc = citySnapshot.docs.find((doc) => doc.id === id);
-  const cityData = cityDoc ? cityDoc.data() : null;
-  return cityData;
+async function getPageDataFromFirestore(db, id) {
+  const database = collection(db, "pages");
+  const documents = await getDocs(database);
+  const document = documents.docs.find((doc) => doc.id === id);
+  const data = document ? document.data() : null;
+
+  if (data) {
+    return { data };
+  } else {
+    return { error: "Page not found" };
+  }
 }
 
-export default async function Home({ params, searchParams }) {
-  const firestoreData = await fetchFromFirestore(db, params.slug);
+export default async function Page({ params }) {
+  const pageId = params.slug;
+  const firestoreData = await getPageDataFromFirestore(db, pageId);
 
   return (
     <div>
-      <h1>Params Data</h1>
+      <h1>Params</h1>
       <pre>{JSON.stringify(params, null, 2)}</pre>
-      <h1>Search Params Data</h1>
-      <pre>{JSON.stringify(searchParams, null, 2)}</pre>
-      <h1>Firestore Data</h1>
+      <h1>Firestore</h1>
       <pre>{JSON.stringify(firestoreData, null, 2)}</pre>
     </div>
   );
