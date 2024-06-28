ce1a4b4904da948cfee5ec23137ff3a2a1e43fba
Leo Mancini <mancini@leo.gd>
2024-06-28 17:38:15 -0400

    prevents caching

--- src/app/page/[slug]/page.js
+++ src/app/page/[slug]/page.js
@@ -1,5 +1,5 @@
 // app/page.js (or any other page file)
-import { db } from "../firebase-config";
+import { db } from "../../../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
 
 async function fetchFromFirestore(db) {
@@ -9,11 +9,15 @@ async function fetchFromFirestore(db) {
   return cityList;
 }
 
-export default async function Home() {
+export default async function Home({ params, searchParams }) {
   const firestoreData = await fetchFromFirestore(db);
 
   return (
     <div>
+      <h1>Params Data</h1>
+      <pre>{JSON.stringify(params, null, 2)}</pre>
+      <h1>Search Params Data</h1>
+      <pre>{JSON.stringify(searchParams, null, 2)}</pre>
       <h1>Firestore Data</h1>
       <pre>{JSON.stringify(firestoreData, null, 2)}</pre>
     </div>
